388f68ded01d2b240b8ccbf18af57a8690e2dc72
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-05-25 01:15:54 +0100

    Add files via upload

---
+++ if.js
@@ -0,0 +1,14 @@
+document.write('hello')
+
+
+console.log("Bonjour JS");
+
+let x = 0
+
+if (x > 0) {
+    console.log("negatif");
+} else if (x < 0) {
+    console.log("positif");
+} else if (x == 0){
+    console.log("egal a 0");
+}
