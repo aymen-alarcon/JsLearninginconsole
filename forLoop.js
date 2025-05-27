dc8a5aff883dda2c7468d45593bc03e89b960fed
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-05-27 05:59:17 +0100

    Add files via upload

---
+++ forLoop.js
@@ -0,0 +1,15 @@
+
+for (let index = 1; index < 12; index++) {
+    console.log(index);   
+}
+
+for (let index = 1; index < 12; index = index + 10) {
+    console.log(index);   
+}
+
+let somme = 0;
+
+for (let index = 3; index <= 33; index++) {
+    somme += index;       
+}
+console.log(somme);
