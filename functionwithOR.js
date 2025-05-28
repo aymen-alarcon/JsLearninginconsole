c19c1c8038728b5856cc58f55a1faec96f3a6ac8
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-05-28 14:06:31 +0100

    Add files via upload

---
+++ functionwithOR.js
@@ -0,0 +1,13 @@
+
+let num4 = 5;
+let num5 = 22;
+
+function checknumbers(num4, num5) {
+    if (num4 <0 || num5 <0) {
+        console.log("le produit des entiers est negatif")
+    }
+    else if(num4 > 0 || num5 > 0){
+        console.log("le produit des entiers est positif");
+    }
+}
+checknumbers(num4, num5);
