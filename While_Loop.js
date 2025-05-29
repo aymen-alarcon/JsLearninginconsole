6772f22c65f2dd4b83f357bd7a9a0611dd1fd337
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-05-29 13:18:53 +0100

    Add files via upload

---
+++ While_Loop.js
@@ -0,0 +1,15 @@
+
+
+function sommeboucle() {
+    
+    let count = 1; 
+    let total=0;
+    while (count <=7) {
+        let x = prompt ("sai")
+        total +=x;
+        count++;
+    }    
+    console.log(total);
+    
+}
+sommeboucle();
